9242f315cfd6740b5f6248bc9c6a304de52d29b5
xintru <52363285+xintru@users.noreply.github.com>
2019-09-09 02:02:28 +0700

    https fix

--- src/store/sagas/productSaga.js
+++ src/store/sagas/productSaga.js
@@ -6,7 +6,7 @@ import * as actions from '../actions/actions';
 export function* productSaga (action) {
     yield put(actions.initStartLoading());
     try {
-        const response = yield axios.get('http://5d22b7fd4e05c600146ef4dd.mockapi.io/cupcake/books')
+        const response = yield axios.get('https://5d22b7fd4e05c600146ef4dd.mockapi.io/cupcake/books')
         yield put(actions.initSuccess(response));
     } catch (error) {
         yield put(actions.initFail(error));
