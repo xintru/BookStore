ea7844189e9247e9e4866c1b34b54d3ff6232e75
xintru <52363285+xintru@users.noreply.github.com>
2019-09-09 01:59:45 +0700

    Updates

---
+++ precache-manifest.82f861e1e2288b153d8d4c0f2f61f1d9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f47d0e53fd6e749dddfac251caae799",
+    "url": "/BookStore/index.html"
+  },
+  {
+    "revision": "9318c5945c08021084c6",
+    "url": "/BookStore/static/css/main.1887e7a8.chunk.css"
+  },
+  {
+    "revision": "b13bcf1a7a4108b69623",
+    "url": "/BookStore/static/js/2.5965ce4e.chunk.js"
+  },
+  {
+    "revision": "9318c5945c08021084c6",
+    "url": "/BookStore/static/js/main.feaeb97c.chunk.js"
+  },
+  {
+    "revision": "4412518145f29a42a427",
+    "url": "/BookStore/static/js/runtime~main.09745ac1.js"
+  }
+]);
